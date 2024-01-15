a8bf1dac9d7a1d9b9269070852190d239fec3940
cheezeit22 <cheezeit22@inha.edu>
2024-01-15 13:21:50 +0900

    v0.7

--- day02/day02.py
+++ day02/day02.py
@@ -53,4 +53,9 @@ print(type(cash)) #int
 
 base_number = int(input('Input base number: '))
 exponent_number = int(input('Input exponent number: '))
+#f-string
 print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number ** exponent_number}')
+print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 pow{{base_number,exponent_number}}')
+
+#format function
+print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(*args: base_number, exponent_number, pow{base_number,exponent_number}))
