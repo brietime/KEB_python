3bf098743cb86275f70e59fe851330c293401dde
cheezeit22 <cheezeit22@inha.edu>
2024-01-15 11:51:12 +0900

    v0.5

--- day02/day02.py
+++ day02/day02.py
@@ -42,3 +42,15 @@ groups = artists
 print(artists, groups)
 artists[2] = '세븐틴'
 print(artists, groups)
+
+money = 5,000,000
+print(money)
+print(type(money)) #tuple
+
+cash = 5_000_000
+print(cash)
+print(type(cash)) #int
+
+base_number = int(input('Input base number: '))
+exponent_number = int(input('Input exponent number: '))
+print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number ** exponent_number}')
