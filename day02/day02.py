8b12f7299cc79fbbe4b0121bd5266a9339fb4859
cheezeit22 <cheezeit22@inha.edu>
2024-01-15 14:13:50 +0900

    v0.9

--- day02/day02.py
+++ day02/day02.py
@@ -67,3 +67,13 @@ quotient = first_number // second_number
 remainder = first_number % second_number
 print(f'몫은 {quotient} 나머지는 {remainder}입니다.')
 print(f'몫은 {divmod(first_number, second_number)[0]}, 나머지는 {divmod(first_number, second_number)[1]}입니다.')
+
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
+print(ord('B'), ord('Z'),ord('a'),ord('2'))
+
