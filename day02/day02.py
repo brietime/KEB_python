9a5663f938ab06be9a91551e050e7c39b69f80b8
cheezeit22 <cheezeit22@inha.edu>
2024-01-15 10:57:21 +0900

    v0.4

--- day02/day02.py
+++ day02/day02.py
@@ -11,10 +11,20 @@ print(subjects)
 print(subjects[3])
 
 subjects[3] = 'data structure & algorithm' #mutable
-print(subjects)
+print(subjects[3])
 
 # frozenset is not mutable 
 
 # print(0.1)
 # print(1e-1)
 # print(21000)
+
+abc = 7
+Abc = 8
+ABC = 6 # case sensitive in lower & upper case letters
+
+test9 = 77
+# 9test = 77 #no num can start as var
+_9test =71 #okay in underline
+
+# False = 123 #no keywords 
