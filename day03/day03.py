57c77f1e2b8fcd28859a1a850d7a61699096a1cf
cheezeit22 <cheezeit22@inha.edu>
2024-01-16 09:58:27 +0900

    v1.5

---
+++ day03/day03.py
@@ -0,0 +1,28 @@
+poem = "'It's preety cold!' said the professor."
+print(poem)
+
+# print(not like thee, Doctor Fell:\n
+#       I know, and know full well:\n) 줄바꿈 \n
+
+
+print('Give', "us", '''some''', """space""") # Give us some space
+
+print(chr(65)) # A
+print(ord('A')) # 65
+
+print(str(True)) # 'True'
+print(str(1.0e4)) # '10000.0'
+
+
+print('ab\tc') # ab      c
+
+info = r'Type a \n key words' #raw string
+print(info)
+university = "Inha\nuniversity"
+print(university)
+universityr = r"Inha\nuniversity"
+print(universityr)
+
+number1 = input("First number: ")
+number2 = input("Second number: ")
+print(number1 + number2) # concatation
