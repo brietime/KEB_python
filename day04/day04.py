a715fae6c838443055e02fa92de1087d7f9c577a
cheezeit22 <cheezeit22@inha.edu>
2024-01-17 14:56:51 +0900

    v3.2

--- day04/day04.py
+++ day04/day04.py
@@ -278,3 +278,22 @@ for k in sugang.keys():
     print(k)
 for v in sugang.values():
     print(v)
+for s in sugang.items():
+    print(s)
+    
+# 안주 추천
+drinks_foods= {'고량주':'양꼬치','위스키':'초콜릿','와인':'치즈','소주':'삼겹살'}
+drinks_foods_keys = list(drinks_foods)
+drink = input(f'다음 술 중에 고르세요.\n1){drinks_foods_keys[0]}, 2){drinks_foods_keys[1]}, 3){drinks_foods_keys[2]}, 4){drinks_foods_keys[3]}, 5)종료')
+if menu == '1':
+    print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]}입니다.')
+elif menu == '2':
+    print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]}입니다.')
+elif menu == '3':
+    print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]}입니다.')
+elif menu == '4':
+    print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]}입니다.')
+elif menu == '5':
+    print('다음에 또 오세요')
+    break
+    
