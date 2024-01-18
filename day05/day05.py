01a9af288d535d2a8a24241e85f59fe09e9c10cc
cheezeit22 <cheezeit22@inha.edu>
2024-01-18 14:24:40 +0900

    v3.7

--- day05/day05.py
+++ day05/day05.py
@@ -259,3 +259,93 @@ def squares(*n):
 
 print(run_function(squares(7,5,2)))
 print(run_function(squares,9,10))
+
+#closure
+def out_func(nout):
+    # new*
+    def inner_func():
+        return nout * nout
+    reutrn inner_func()
+
+x = out_func(9)
+print(type(x))
+print(x)
+print(x())
+
+# Inner function
+def out_func(nout):
+    new*
+    def inner_func(nin):
+        return nin *nin
+    reutrn inner_func(nout)
+
+print(out_func(5))
+
+#lambda func
+numbers = ["7","-11","3"]
+print(sum(map(int,numbers)))
+          
+          
+# numbers = ["7","-11","3"]
+# hap = 0
+# for number in numbers:
+#     hap += int(number)
+# print(hap)
+
+# menu with func version2
+def isprime(n) -> bool:
+    """
+    매개변수로 넘겨받은 수가 소수인지 여부를 boolean으로 리턴
+    :param n: 판정할 매개변수
+    :return: 소수면 True, 소수가 아니면 False
+    """
+    if n < 2:
+        return False
+    else:
+        i=2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1   
+        return True      
+    
+while True:
+    menu = input("1) fahrenheit -> celsius  2) Celsius -> fahrenheit  3) Prime number  4) Prime numbers(two input)  5) Quit Program : ")
+    
+    if menu  == '1':
+        fahrenheit = float(input('Input fahrenheit: '))
+        print(f'Fahrenheit: {fahrenheit}F, Celsius: {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+    
+    elif menu == '2':
+        celsius = float(input('Input Celcius: '))
+        print(f'Celsius: {celsius}C, fahrenheit: {((celsius *9.0/5.0)+32.0):.4f}F')
+    
+    elif menu == '3':
+        number = int(input("Input number: "))
+        for i in range(2, number): 
+            if isprime(number):
+                print(f'{number} is prime number.')
+            else: 
+                print(f'{number} is NOT prime number')
+        
+    elif menu == '4':
+        n1,n2 = map(int,input("first number Second number: ").split()) #map, int?에 함수의 이름을 넣음
+        n1,n2 = min(n1,n2), max(n1,n2) #unpack
+        # n1 = int(numbers[0])
+        # n2 = int(numbers[1])
+
+        if n1 > n2:
+            n1,n2 = n2,n1
+            
+        for number in range(n1,n2+1,1):
+            if isprime(number):
+                print(number, end = ' ')
+        print()
+    
+    elif menu == '5':
+        print('Terminate Program.')
+        break
+    
+    else: 
+        print('Invaild Syntax!')
+        
