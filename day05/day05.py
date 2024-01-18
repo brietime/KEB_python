dd57c8eb9a7fc5081f5e0672efe90565924dc012
cheezeit22 <cheezeit22@inha.edu>
2024-01-18 15:50:59 +0900

    v3.8

--- day05/day05.py
+++ day05/day05.py
@@ -281,7 +281,7 @@ def out_func(nout):
 
 print(out_func(5))
 
-#lambda func
+
 numbers = ["7","-11","3"]
 print(sum(map(int,numbers)))
           
@@ -348,4 +348,70 @@ while True:
     
     else: 
         print('Invaild Syntax!')
-        
+    
+# lambda func.
+
+def squares(n):
+    return n * n
+
+even_numbers = [i for i in range(51) if i % 2 == 0]
+print(even_numbers)
+print(tuple(map(squares, even_numbers)))
+print(tuple(map(lambda x: x**2, even_numbers))) 
+
+z = lambda x:pow(x ,2)
+print(tuple(map(z ,even_numbers)))
+
+sum(range(1,101))
+
+#generator
+def my_range(first=0,last=10,step=1):
+    number = first
+    while number <last:
+        yield number # 여기로 발생시켜서 보냄
+        number += step
+r = my_range()
+print(r, type(r))
+
+for x in r:
+    print(x)
+for x in r:# 한번만 출력됨 - 저장되지 않고 출력후 버려짐
+    print(x)
+    
+
+# decorator
+def description(f):  # closure
+    def inner(*args):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*args)
+        return r
+
+    return inner
+
+
+def squares(n):
+    """
+    제곱 함수
+    """
+    return n * n
+
+@description
+def power(b, e):
+    """
+    거듭제곱 함수
+    """
+    result = 1
+    for _ in range(e):
+        result = result * b
+    return result
+
+f1 = description(squares)
+print(f1(9))
+print(power(2, 10))
+# f2 = description(power)
+# print(f2(2, 10))
+
+# print(squares(7))
+# print(squares.__doc__)
+
